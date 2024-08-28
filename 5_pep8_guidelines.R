4edb245378af858fa52cec8669540f0cc1c5ee7a
casey-hansen <112713946+casey-hansen@users.noreply.github.com>
2024-08-28 11:15:08 -0400

    Add citation examples

--- 5_pep8_guidelines.R
+++ 5_pep8_guidelines.R
@@ -9,8 +9,15 @@
 time <- c(1:50)
 
 # temperature data, generate 50 random numbers between 90 and 100
+# sample() function sourced from Geeks for Geeks [1]
 temp <- sample(90.0:100.0, 50, replace = TRUE)
 
 #plot the data to observe temperature over time
 # set markers to blue for easier visualization, pch = 19 for solid
+# pch argument sourced from ChatGPT [2]
 plot(time, temp, col = 'blue', pch = 19)
+
+
+### Citations ###
+# [1] https://www.geeksforgeeks.org/how-to-generate-random-numbers-in-r/
+# [2] ChatCPT version GPT-4, Query: "what are the options for plot markers in R"
